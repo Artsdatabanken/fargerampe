7900278395541f68ee458e43f546739dd1ab1cc3
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-25 15:07:05 +0100

    Support alpha (transparency)

--- io.js
+++ io.js
@@ -14,13 +14,15 @@ function writeJson(fn, o) {
 function writePng(cmap, pngFilePath) {
   var png = new PNG({ width: 256, height: 16 });
   for (var x = 0; x < png.width; x++) {
-    const color = new tinycolor(cmap[x]);
+    const farge = cmap[x];
+    console.log(new tinycolor(farge)._a);
+    const color = new tinycolor(farge ? farge : "rgba(255,255,255,0)");
     for (var y = 0; y < png.height; y++) {
       var idx = (png.width * y + x) << 2;
       png.data[idx] = color._r;
       png.data[idx + 1] = color._g;
       png.data[idx + 2] = color._b;
-      png.data[idx + 3] = 255;
+      png.data[idx + 3] = color._a * 255;
     }
   }
 
